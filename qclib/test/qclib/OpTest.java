89606fdfb61fa703162269f28ce9dad5a427aaa3
Dylan Hutchison <dhutchis@stevens.edu>
2014-02-04 11:01:05 +0000

    Add one quick test.

--- qclib/test/qclib/OpTest.java
+++ qclib/test/qclib/OpTest.java
@@ -10,10 +10,10 @@ import org.junit.Test;
 
 import qclib.util.QuantumUtil;
 
-public class ZTest {
+public class OpTest {
 
 	@Test
-	public void testApply() {
+	public void testZ() {
 		Complex c0 = new Complex(0,Math.sqrt(0.5));
 		Complex c1 = new Complex(Math.sqrt(0.75),0);
 		
@@ -25,6 +25,27 @@ public class ZTest {
 		
 		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(0), c0)  );
 		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(1), c1.negate())  );
+	}
+	
+	@Test
+	public void testCNOT() {
+		Complex c0 = new Complex(0,1/3.0),
+				c1 = new Complex(Math.sqrt(1/6.0),0),
+				c2 = new Complex(2/3.0,0),
+				c3 = new Complex(0,Math.sqrt(5/18.0));
+		
+		FieldVector<Complex> vec = new ArrayFieldVector<Complex>(ComplexField.getInstance(),4);
+		vec.setEntry(0, c0);
+		vec.setEntry(1, c1);
+		vec.setEntry(2, c2);
+		vec.setEntry(3, c3);
+		
+		FieldVector<Complex> outvec = new CNOT().apply(vec);
+		
+		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(0), c0)  );
+		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(1), c1)  );
+		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(2), c3)  );
+		assertTrue( QuantumUtil.isApproxEqualComplex(outvec.getEntry(3), c2)  );
 	}
 
 }
