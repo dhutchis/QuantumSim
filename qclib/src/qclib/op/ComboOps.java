cde2ce15369e7d5b217724663cee2dc204cf92f9
Dylan Hutchison <dhutchis@stevens.edu>
2014-02-12 23:14:53 +0000

    Fixed Toffoli.

--- qclib/src/qclib/op/ComboOps.java
+++ qclib/src/qclib/op/ComboOps.java
@@ -12,14 +12,14 @@ public class ComboOps {
 		Operator H2 = new H().extend(3, 2);
 		Operator CV12 = new CV().extend(3, 1, 2);
 		Operator CV02 = new CV().extend(3, 0, 2);
-		Operator CNOT01 = new CNOT().extend(3, 0, 1);
-		Operator newop = H2.curryBefore(CV02)
+		Operator CNOT01 = new CNOT().extend(3, 1, 0);
+		Operator newop = H2.curryBefore(CV12)
 				.curryBefore(CNOT01)
 				.curryBefore(CV12)
 				.curryBefore(CV12)
 				.curryBefore(CV12)
 				.curryBefore(CNOT01)
-				.curryBefore(CV12)
+				.curryBefore(CV02)
 				.curryBefore(H2);
 		return newop;
 	}
